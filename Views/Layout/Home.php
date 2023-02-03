4128afa43b653f1e8292117b88dc0667cd4a1c48
Mauro Miguel <32025338+MauCoden95@users.noreply.github.com>
2023-02-02 22:02:54 -0300

    Add files via upload

--- Views/Layout/Home.php
+++ Views/Layout/Home.php
@@ -1,24 +1,84 @@
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="<?= base_url ?>Assets/StylesHome.css">
-        <script src="https://kit.fontawesome.com/7483adbd94.js" crossorigin="anonymous"></script>
-        <title>AsistBox</title>
-    </head>
-    <body>
-        <header id="header">
-            <h1>Bienvenido <?php print_r($_SESSION['identity']->username); ?></h1>
-        </header>
+<html lang="en">
 
-        <section class="home-section">
-            <div id="navbar">
-                <img src="<?= base_url ?>Assets/Img/Logo.png" alt="">
-                <a href="<?= base_url ?>Admin/login">Cerrar Sesión</a>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url ?>Assets/StylesHome.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/7483adbd94.js" crossorigin="anonymous"></script>
+    <title>AsistBox</title>
+</head>
+
+<body>
+    <header id="header">
+        <h1>Bienvenido
+            <?php print_r($_SESSION['identity']->username); ?>
+        </h1>
+    </header>
+
+    <section class="home-section">
+        <div id="navbar">
+            <img src="<?= base_url ?>Assets/Img/Logo.png" alt="">
+
+            <div class="accordion accordion-flush w-75 m-auto" id="accordionFlushExample">
+                <div class="accordion-item mb-3">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed fs-4" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            Empleados
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body fs-5">Listado de empleados</div>
+                    </div>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion
+                            body.</div>
+                    </div>
+                </div>
+                <div class="accordion-item mb-3">
+                    <h2 class="accordion-header" id="flush-headingTwo">
+                        <button class="accordion-button collapsed fs-4" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            Cargos
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body fs-5">Listado de cargos</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseThree" aria-expanded="false"
+                            aria-controls="flush-collapseThree">
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div id="flush-collapseThree" class="accordion-collapse collapse"
+                        aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion
+                            body. Nothing more exciting happening here in terms of content, but just filling up the
+                            space to make it look, at least at first glance, a bit more representative of how this would
+                            look in a real-world application.</div>
+                    </div>
+                </div>
             </div>
-        </section>
-    </body>
-</html>
+            <a href="<?= base_url ?>Admin/login">Cerrar Sesión</a>
+        </div>
+    </section>
 
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
